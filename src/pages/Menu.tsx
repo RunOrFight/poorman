4e140abd3ce63e92d4577477806782f257728fcd
Ignat Mustafin <ignatmustafin77@gmail.com>
2023-06-19 01:21:37 +0300

    #1 added signalR connection

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { Button, InfiniteProgress, Input, Window } from "../ui";
 import { useCreateLobby, useJoinLobby } from "../services";
-import { useCallback, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 import { WaitForLobby } from "../widgets";
-import useWebSocket from "react-use-websocket";
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
-const WS_URL = "ws://localhost:5157";
+
+// const WS_URL = "ws://localhost:3000/socket.io";
 
 const MenuPage = () => {
+  const [connection, setConnection] = useState(null);
   const navigate = useNavigate();
   const [isWindowVisible, setIsWindowVisible] = useState(false);
   const [lobbyIdInputText, setLobbyIdInputText] = useState("");
@@ -15,15 +17,41 @@ const MenuPage = () => {
   const { data, requestStatus, createLobby } = useCreateLobby();
   const { joinLobby } = useJoinLobby();
 
-  useWebSocket(WS_URL, {
-    onOpen: () => {
-      console.log("WebSocket connection established.");
-    },
+  useEffect(() => {
+    const newConnection = new HubConnectionBuilder()
+        .withUrl('http://localhost:5157/socket') // Замените на правильный URL вашего сервера SignalR
+        .withAutomaticReconnect()
+        .build();
 
-    onMessage: (e) => {
-      console.log(e);
-    },
-  });
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    setConnection(newConnection);
+
+  }, [])
+
+  useEffect(() => {
+    if (connection) {
+      connection.start()
+          .then(() => {
+            console.log('Connected to the server.');
+            console.log(JSON.parse(localStorage.user).id)
+            connection.invoke("SetUserId", JSON.parse(localStorage.user).id)
+                .then(() => {
+                  console.log("SetUserId invoked successfully.");
+                })
+                .catch((error) => {
+                  console.error("Failed to invoke SetUserId:", error);
+                });
+          })
+          .catch((error) => {
+            console.error('Failed to connect to the server: ', error);
+          });
+
+      connection.on("all_users_joined_lobby", () => {
+        console.log("All users joined lobby")
+      })
+    }
+  }, [connection]);
 
   const handleCreateLobbyButtonClick = useCallback(() => {
     setIsWindowVisible(true);
