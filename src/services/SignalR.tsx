73ff4597080295454fb8e4011e6d80d874585cf8
RunOrFight <dragoinfury@gmail.com>
2023-06-20 17:25:57 +0300

    refactor: SignalR

--- src/services/SignalR.tsx
+++ src/services/SignalR.tsx
@@ -6,7 +6,7 @@ import {
   useContext,
   useEffect,
   useRef,
-  useState,
+  useState, useCallback,
 } from "react";
 import { useAuth } from ".";
 import { InfiniteProgress } from "../ui";
@@ -16,40 +16,39 @@ const SignalRContext = createContext<HubConnection>(null!);
 const SignalRProvider: FC<PropsWithChildren> = ({ children }) => {
   const [isConnected, setIsConnected] = useState(false);
   const connection = useRef<HubConnection | null>(null);
+  const {user} = useAuth()
+  const startConnection = useCallback(async (connection: HubConnection) => {
+  try {
+    await connection.start();
+    console.log("Connected to the server.");
+    await connection.invoke("SetUserId", user?.id);
+    console.log("SetUserId invoked successfully.");
+    setIsConnected(true);
+  } catch (error) {
+    console.error("Failed to start or invoke SetUserId:", error);
+  }
+}, [user?.id]);
 
-  useEffect(() => {
-    if (!connection.current) {
-      connection.current = new HubConnectionBuilder()
-        .withUrl("http://localhost:5157/socket")
-        .withAutomaticReconnect()
-        .build();
-      connection.current
-        .start()
-        .then(() => {
-          console.log("Connected to the server.");
-          connection
-            .current!.invoke("SetUserId", user?.id)
-            .then(() => {
-              console.log("SetUserId invoked successfully.");
-              setIsConnected(true);
-            })
-            .catch((error) => {
-              console.error("Failed to invoke SetUserId:", error);
-            });
-        })
-        .catch((error) => {
-          console.error("Failed to connect to the server: ", error);
-        });
-    }
+useEffect(() => {
+  if (!connection.current) {
+    connection.current = new HubConnectionBuilder()
+      .withUrl("http://localhost:5157/socket")
+      .withAutomaticReconnect()
+      .build();
+    connection.current.onclose(() => {
+      console.log("Connection closed.");
+      setIsConnected(false);
+    });
+  }
 
-    () => {
-      connection.current?.stop().then(() => {
-        console.log("Terminated");
-      });
-    };
-  }, []);
+  startConnection(connection.current);
 
-  const { user } = useAuth();
+  return () => {
+    connection.current?.stop().then(() => {
+      console.log("Terminated");
+    });
+  };
+}, [startConnection]);
 
   return (
     <SignalRContext.Provider value={connection.current!}>
