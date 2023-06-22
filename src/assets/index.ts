4fb259c6cad394e7c67aba924f336b937eefc6fa
RunOrFight <dragoinfury@gmail.com>
2023-06-22 23:52:59 +0300

    refactor: card type

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,18 @@ import piece from "../assets/piece.png";
 import hero from "../assets/hero.png";
 import divider from "../assets/divider.png";
 import progress from "../assets/progress.png";
+import blue_card_back from "../assets/blue_card_back.png";
+import green_card_back from "../assets/green_card_back.png";
+import red_card_back from "../assets/red_card_back.png";
+import yellow_card_back from "../assets/yellow_card_back.png";
 
-export { piece, hero, divider, progress };
+export {
+  piece,
+  hero,
+  divider,
+  progress,
+  blue_card_back,
+  green_card_back,
+  red_card_back,
+  yellow_card_back,
+};
--- src/ui/Card/Card.tsx
+++ src/ui/Card/Card.tsx
@@ -2,28 +2,38 @@ import { FC } from "react";
 import classes from "./Card.module.css";
 import { IPlayerCard, CardType } from "../../interfaces";
 import clsx from "clsx";
+import {
+  blue_card_back,
+  green_card_back,
+  red_card_back,
+  yellow_card_back,
+} from "../../assets";
 
 const Card: FC<IPlayerCard> = ({ id, type, name, ...rest }) => {
   let typeBg;
-
+  let backUrl;
   switch (type) {
     case CardType.All:
       typeBg = "bg-yellow-400";
+      backUrl = yellow_card_back;
       break;
     case CardType.Left:
       typeBg = "bg-blue-400";
+      backUrl = blue_card_back;
       break;
     case CardType.Right:
       typeBg = "bg-green-400";
+      backUrl = green_card_back;
       break;
     case CardType.Straight:
       typeBg = "bg-red-400";
+      backUrl = red_card_back;
       break;
     default:
       typeBg = "bg-grey-400";
   }
 
-  return (
+  return id ? (
     <div
       className={clsx(
         classes.card,
@@ -33,6 +43,11 @@ const Card: FC<IPlayerCard> = ({ id, type, name, ...rest }) => {
     >
       <div className="text-white ">{name}</div>
     </div>
+  ) : (
+    <div
+      className={clsx(classes.card, "bg-no-repeat bg-contain bg-transparent")}
+      style={{ backgroundImage: `url(${backUrl})` }}
+    ></div>
   );
 };
 
