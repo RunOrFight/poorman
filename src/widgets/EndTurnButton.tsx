3c13f1861285ed811edf703b22e4dadc9b90ab05
RunOrFight <dragoinfury@gmail.com>
2023-06-23 10:47:38 +0300

    feat: fix card style

--- src/widgets/EndTurnButton.tsx
+++ src/widgets/EndTurnButton.tsx
@@ -1,37 +1,37 @@
-import {useEffect, useState} from "react";
-import {useTimer} from "../services";
-import {Button} from "../ui";
-import {useAppSelector} from "../store";
-import {useEndTurnMutation} from "../api";
+import { useEffect, useState } from "react";
+import { useTimer } from "../services";
+import { Button } from "../ui";
+import { useAppSelector } from "../store";
+import { useEndTurnMutation } from "../api";
 
 const EndTurnButton = () => {
-    // const { timeLeft, isTimeOver, startTimer, stopTimer } = useTimer(30);
-    const [isTurnEnd, setIsTurnEnd] = useState(false);
-    const playerId = useAppSelector(state => state.game.playerId);
-    const [endTurn] = useEndTurnMutation();
+  // const { timeLeft, isTimeOver, startTimer, stopTimer } = useTimer(30);
+  const [isTurnEnd, setIsTurnEnd] = useState(false);
+  const playerId = useAppSelector((state) => state.game.playerId);
+  const [endTurn] = useEndTurnMutation();
 
-    // useEffect(() => {
-    //   startTimer();
-    // }, []);
+  // useEffect(() => {
+  //   startTimer();
+  // }, []);
 
-    // useEffect(() => {
-    //   isTimeOver && setIsTurnEnd(true);
-    // }, [isTimeOver]);
+  // useEffect(() => {
+  //   isTimeOver && setIsTurnEnd(true);
+  // }, [isTimeOver]);
 
-    const handleClick = () => {
-        // stopTimer();
-        setIsTurnEnd(true);
-        endTurn({playerId})
-    };
+  const handleClick = () => {
+    // stopTimer();
+    setIsTurnEnd(true);
+    endTurn({ playerId });
+  };
 
-    return (
-        <div className="flex flex-col items-center gap-2">
-            {/*<div className="text-white font-sans text-xl">{timeLeft} sec</div>*/}
-            <Button disabled={isTurnEnd} onClick={handleClick}>
-                End Turn
-            </Button>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center gap-2">
+      {/*<div className="text-white font-sans text-xl">{timeLeft} sec</div>*/}
+      <Button disabled={isTurnEnd} onClick={handleClick}>
+        {isTurnEnd ? "Waiting for battle" : "End Turn"}
+      </Button>
+    </div>
+  );
 };
 
 export default EndTurnButton;
