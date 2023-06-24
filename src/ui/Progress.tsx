9c5f58e1822487b4f636c0b53af65ea1a1d8f84f
Ignat Mustafin <ignatmustafin77@gmail.com>
2023-06-24 23:58:41 +0300

    #0 end turn

--- src/ui/Progress.tsx
+++ src/ui/Progress.tsx
@@ -2,13 +2,13 @@ import { piece, progress } from "../assets";
 import { usePlayerSelector } from "../store";
 
 const Progress = () => {
-  const mana = usePlayerSelector().mana;
+  const manaCurrent = usePlayerSelector().manaCurrent;
   return (
     <div
       style={{ backgroundImage: `url(${progress})` }}
       className="w-[70px] bg-center relative flex justify-end flex-col  bg-contain h-[492px] bg-no-repeat px-[2px]"
     >
-      {new Array(mana).fill(null).map((i, index) => {
+      {new Array(manaCurrent).fill(null).map((i, index) => {
         const bottom = `${12 + index * 24}px`;
         const zIndex = `${50 - index}`;
         return (
