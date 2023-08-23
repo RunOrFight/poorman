f4c9a783a13a79b1578db3a080afe22f74939571
NikitaShakhovich <7149382@tut.by>
2023-08-23 13:25:43 +0300

    feat: create html structure

--- src/pages/Tutorial/index.tsx
+++ src/pages/Tutorial/index.tsx
@@ -66,44 +66,192 @@ const Index = () => {
         </div>
       </section>
       <section>
-        <div className={clsx(styled.container)}>
-          <div className={styled.styled.cardLevelDescription}>
+        <div className={styled.cardLevelContainer}>
+          <div className={styled.cardLevelDescription}>
             <h2 className={styled.h2}>Card level</h2>
             <p className={styled.text}>3 breeds of cats and 3 dogs indicate the level of the character. This affects the cost of the card, the amount of damage and health of the character</p>
           </div>
-          <div>
+          <div className={styled.levelList}>
             <div>
-              <img src='/src/assets/tutorial/card_level_1.png' alt='level-1' />
-              <p></p>
-              <p></p>
+              <img className={styled.cardsDeckImg} src='/src/assets/tutorial/card_level_1.png' alt='level-1' width="193px" height="265px"/>
+              <p>1 lvl</p>
+              <p>Munchkin</p>
             </div>
             <div>
-              <img src='/src/assets/tutorial/card_level_1.png' alt='level-1' />
-              <p></p>
-              <p></p>
+              <img className={styled.cardsDeckImg} src='/src/assets/tutorial/card_level_2.png' alt='level-2' width="193px" height="265px"/>
+              <p>2 lvl</p>
+              <p>Maine coon</p>
             </div>
             <div>
-              <img src='/src/assets/tutorial/card_level_1.png' alt='level-1' />
-              <p></p>
-              <p></p>
+              <img className={styled.cardsDeckImg} src='/src/assets/tutorial/card_level_3.png' alt='level-3' width="193px" height="265px"/>
+              <p>3 lvl</p>
+              <p>Sphinx</p>
             </div>
             <div>
-              <img src='/src/assets/tutorial/card_level_1.png' alt='level-1' />
-              <p></p>
-              <p></p>
+              <img className={styled.cardsDeckImg} src='/src/assets/tutorial/card_level_4.png' alt='level-4' width="193px" height="265px"/>
+              <p>4 lvl</p>
+              <p>Corgi</p>
             </div>
             <div>
-              <img src='/src/assets/tutorial/card_level_1.png' alt='level-1' />
-              <p></p>
-              <p></p>
+              <img className={styled.cardsDeckImg} src='/src/assets/tutorial/card_level_5.png' alt='level-5' width="193px" height="265px"/>
+              <p>5 lvl</p>
+              <p>terrier</p>
             </div>
             <div>
-              <img src='/src/assets/tutorial/card_level_1.png' alt='level-1' />
-              <p></p>
-              <p></p>
+              <img className={styled.cardsDeckImg} src='/src/assets/tutorial/card_level_6.png' alt='level-6' width="193px" height="265px"/>
+              <p>6 lvl</p>
+              <p>Husky</p>
             </div>
           </div>
         </div>
+      </section>
+
+      <section>
+        <div>
+          <div className={styled.cardClassDescription}>
+            <h2 className={styled.h2}>Card class</h2>
+            <p className={styled.text}>Depending on the weapon, the cards attack in different directions</p>
+          </div>
+        </div>
+        <div className={styled.cardClassList}>
+          <div>
+            <p>Red warrior</p>
+            <p>forward</p>
+            <img className={styled.cardsDeckImg} src='/src/assets/tutorial/class_red.png' alt='class_red' width="193px" height="265px" />
+          </div>
+          <div>
+            <p>Blue archer</p>
+            <p>forward and to the left</p>
+            <img className={styled.cardsDeckImg} src='/src/assets/tutorial/class_blue.png' alt='class_blue' width="193px" height="265px" />
+          </div>
+          <div>
+            <p>Green shooter</p>
+            <p>forward and to the right</p>
+            <img className={styled.cardsDeckImg} src='/src/assets/tutorial/class_green.png' alt='class_green' width="193px" height="265px" />
+          </div>
+          <div>
+            <p>Yellow electric</p>
+            <p>all three directions</p>
+            <img className={styled.cardsDeckImg} src='/src/assets/tutorial/class_yellow.png' alt='class_yellow' width="193px" height="265px" />
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className={styled.gameBoardContainer}>
+          <div>
+            <h2 className={styled.h2}>Game board</h2>
+            <p className={styled.text}>On the game board, you can find:</p>
+            <ul>
+              <li>Player's name and health</li>
+              <li>Number of energy blocks</li>
+              <li>Card holder</li>
+              <li>Cards in hand</li>
+              <li>Opponent's cards in hand</li>
+              <li>End turn button</li>
+              <li>Fields for placing cards</li>
+            </ul>
+          </div>
+          <div>
+            <img className={styled.gameBoardImg} src='/src/assets/tutorial/game_board.png' alt='game_board' width="875px" height="675px" />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <div className={styled.cardClassDescription}>
+            <h2 className={styled.h2}>Deck of cards</h2>
+            <p className={styled.text}>There are 48 cards in the deck</p>
+            <div>
+              <p>At the beginning of the game, both you and your opponent draw 3 cards from the deck. Then, at the start of each turn, you draw 2 cards</p>
+              <p>You cannot have more than 5 cards in your hand. If the number of cards in your hand exceeds 5 during the card draw phase, any excess cards that were supposed to be drawn are discarded</p>
+            </div>
+          </div>
+          <div className={styled.deckCardList}>
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/munchkin_warrior.png' alt='munchkin_warrior' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/maine_coon_warrior.png' alt='maine_coon_warrior' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/sphinx_warrior.png' alt='sphinx_warrior' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/corgi_warrior.png' alt='corgi_warrior' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/terrier_warrior.png' alt='terrier_warrior' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/husky_warrior.png' alt='husky_warrior' width="166px" height="230px" />
+
+
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/munchkin_archer.png' alt='munchkin_archer' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/maine_coon_archer.png' alt='maine_coon_archer' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/sphinx_archer.png' alt='sphinx_archer' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/corgi_archer.png' alt='corgi_archer' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/terrier_archer.png' alt='terrier_archer' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/husky_archer.png' alt='husky_archer' width="166px" height="230px" />
+
+
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/munchkin_shooter.png' alt='munchkin_shooter' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/maine_coon_shooter.png' alt='maine_coon_shooter' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/sphinx_shooter.png' alt='sphinx_shooter' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/corgi_shooter.png' alt='corgi_shooter' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/terrier_shooter.png' alt='terrier_shooter' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/husky_shooter.png' alt='husky_shooter' width="166px" height="230px" />
+
+
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/munchkin_energy.png' alt='munchkin_energy' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/maine_coon_energy.png' alt='maine_coon_energy' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/sphinx_energy.png' alt='sphinx_energy' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/corgi_energy.png' alt='corgi_energy' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/terrier_energy.png' alt='terrier_energy' width="166px" height="230px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/husky_energy.png' alt='husky_energy' width="166px" height="230px" />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <div className={styled.cardClassDescription}>
+            <h2 className={styled.h2}>Energy Blocks</h2>
+            <p className={styled.text}>You use energy blocks to play cards. At the beginning of the game, you start with 2 energy blocks.
+              With each turn, the number of energy blocks you receive will increase by 1 until it reaches 7</p>
+          </div>
+          <div className={styled.energyBlockImg}>
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/energy_scale.png' alt='energy_scale' width="47px" height="384px" />
+            <img className={styled.deckCardImg} src='/src/assets/tutorial/cost_card.png' alt='cost_card' width="256px" height="384px" />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <div className={styled.cardClassDescription}>
+            <h2 className={styled.h2}>Gameplay</h2>
+            <p className={styled.text}>The turn is divided into 2 phases</p>
+          </div>
+          <ol>
+            <li>1. Card Deployment Phase
+              <div>
+                <p>In the first phase, you and your opponent deploy cards onto the field. Your opponent can infer that you have played a card based on the decrease in the number of cards in your hand. However, they will only see which card you played and on which field during the second phase.</p>
+                <p>To end the first phase, click the "End Turn" button. Once both you and your opponent have completed the first phase, the second phase begins.</p>
+              </div>
+              <div>
+                <img src='/src/assets/tutorial/block_video.png' alt='block_video' width="875px" height="675px" />
+              </div>
+            </li>
+
+            <li>2. Battle Phase
+              <div>
+                <p>In the second phase, the cards battle each other. Cards attack from left to right.</p>
+                <ul>
+                  <li>If a card attacks an empty field, the opponent’s hero takes damage.</li>
+                  <li>If a card attacks another card, the health of the targeted card is reduced by the attack value of the attacking card.</li>
+                  <li>If a card's health reaches 0 or less and it has already attacked, the card dies (a card will not die until it has attacked).</li>
+                </ul>
+                <div>
+                  <p>After all cards have finished attacking, the next turn begins. The game ends when one of the players' health reaches zero.</p>
+                </div>
+              </div>
+              <div>
+                <img src='/src/assets/tutorial/block_video.png' alt='block_video' width="875px" height="675px" />
+              </div>
+            </li>
+          </ol>
+        </div>
+
       </section>
       {/*<section ref={secondSection} className={styled.suggestion}>*/}
       {/*  <div className={styled.block}>*/}
@@ -151,19 +299,19 @@ const Index = () => {
       {/*    PLAY*/}
       {/*  </a>*/}
       {/*</section>*/}
-      {/*<footer className={styled.footer}>*/}
-      {/*  <img*/}
-      {/*    className={clsx(styled.logo, styled.small)}*/}
-      {/*    src={logo}*/}
-      {/*    alt="Cybercats"*/}
-      {/*    width="339px"*/}
-      {/*    height="61px"*/}
-      {/*  />*/}
-      {/*  <a className={styled.mailto} href="mailto:info@cybercats.live">*/}
-      {/*    info@cybercats.live*/}
-      {/*  </a>*/}
-      {/*  <p className={styled.copyright}>© All Rights Reserved</p>*/}
-      {/*</footer>*/}
+      <footer className={styled.footer}>
+        <img
+          className={clsx(styled.logo, styled.small)}
+          src={logo}
+          alt="Cybercats"
+          width="339px"
+          height="61px"
+        />
+        <a className={styled.mailto} href="mailto:arseni@cybercats.live">
+          arseni@cybercats.live
+        </a>
+        <p className={styled.copyright}>© All Rights Reserved</p>
+      </footer>
     </div>
   );
 };
